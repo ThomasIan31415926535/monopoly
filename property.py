5362f382b2c72b44c69b275929650a783be02209
ThomasIan31415926535 <146150707+ThomasIan31415926535@users.noreply.github.com>
2024-11-19 23:55:58 +0800

    Delete property.py

--- property.py
+++
@@ -1,7 +0,0 @@
-# model/property.py
-class Property:
-    def __init__(self, name, price, rent):
-        self.name = name
-        self.price = price
-        self.rent = rent
-        self.owner = None
